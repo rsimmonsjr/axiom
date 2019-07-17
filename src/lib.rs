38751ff9b6cf645f94f671693165030a5855468b
Robert Simmons Jr <44960786+rsimmonsjr@users.noreply.github.com>
2019-07-16 21:04:27 -0500

    Integrating logging and a brand new, hopefully final, package name (#18)

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
-extern crate uuid;
-
 pub mod actors;
 pub mod secc;
 
 #[cfg(test)]
 mod tests {
+    use log::LevelFilter;
+
+    pub fn init_test_log() {
+        let _ = env_logger::builder()
+            .filter_level(LevelFilter::Debug)
+            .is_test(true)
+            .try_init();
+    }
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
