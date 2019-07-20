c5c68c247ad75f1e41d7f919adb7addbc3d72acf
Robert Simmons Jr <kraythe@outlook.com>
2019-07-19 22:12:47 -0500

    Adding a ton of documentation and copy profing and fixing discovered bugs.

--- src/lib.rs
+++ src/lib.rs
@@ -6,27 +6,33 @@
 //! however, not a direct re-implementation of either of the two aforementioned actor models but
 //! rather a new implementation deriving inspiration from the good parts of those models.
 //!
-//! ## What is an Actor Model
+//! # What's New
+//! ### 0.0.3
+//! Additional documentation, resolving a lot of bugs.
 //!
-//! An actor model is an architectural asynchronous programming paradigm characterized by the use
-//! of actors for all processing activities. Actors have the following characteristics:
+//! # Getting Started
+//!
+//! *An actor model is an architectural asynchronous programming paradigm characterized by the use
+//! of actors for all processing activities.*
+//!
+//! Actors have the following characteristics:
 //! 1. An actor can be interacted with only by means of messages.
 //! 2. An actor processes only one message at a time.
 //! 3. An actor will process a message only once.
 //! 4. An actor can send a message to any other actor without knowledge of that actor's internals.
 //! 5. Actors send only immutable data as messages, though they may have mutable internal state.
-//! 6. Actors are location agnostic. An actor can be sent a message from anywhere in the cluster.
+//! 6. Actors are location agnostic; they can be sent a message from anywhere in the cluster.
 //!
-//! Note that for the purposes of Rust, rule five cannot be enforced by this library but is
-//! important for users of the library developing actors based on this library. In Erlang and
-//! Elixir this rule cannot be violated because of the structure of the language but that leads to
-//! limitations as well because Erlang and Elixir actors cannot hold internal mutable state.
+//! Note that within the language of Rust, rule five cannot be enforced by this library but is
+//! important for developers creating actors based on this library. In Erlang and Elixir rule
+//! five cannot be violated because of the structure of the language but this also leads to
+//! performance limitations. It's better to allow internal mutable state and encourage the good
+//! practice of not sending mutable state as messages.
 //!
 //! What is important to understand is that these rules combined together makes each actor operate
 //! like a micro-service in the memory space of the program using them. Since actor messages are
-//! immutable, actors can trade information safely and easily without copying large data structures.
-//!
-//! ## Getting Started
+//! immutable, actors can trade information safely and easily without copying large data
+//! structures.
 //!
 //! Although programming in the actor model is quite an involved process you can get started with
 //! Axiom in only a few lines of code.
@@ -45,10 +51,10 @@
 //! ActorId::send(&aid, Arc::new(11));
 //! ```
 //!
-//! This code creates an an actor system and then spawns an actor and finally sends the actor a
-//! message. That is really all there is to it but of course it doesn't end there. If you want
-//! to create an actor with a struct that is simple as well. Let's create one that handles a
-//! couple of message types.
+//! This code creates an actor system, spawns an actor and finally sends the actor a message.
+//! That is really all there is to it but of course it doesn't end there. If you want to create
+//! an actor with a struct that is simple as well. Let's create one that handles a couple of
+//! different message types:
 //!
 //! ```rust
 //! use axiom::actors::*;
@@ -98,7 +104,7 @@
 //! ```
 //!
 //! This code creates an actor out of an arbitrary struct. Since the only requirement to make
-//! an actor is to have a function that is compliant with the [axiom::actors::Processor] trait,
+//! an actor is to have a function that is compliant with the [`axiom::actors::Processor`] trait,
 //! anything can be an actor. If this struct had been declared somewhere outside of your control
 //! you could use it in an actor as state by declaring your own handler function and making the
 //! calls to the 3rd party structure.
@@ -106,11 +112,11 @@
 //! *It's important to keep in mind that the starting state is moved into the actor and you
 //! will not have external access to it afterwards.* This is by design and although you could
 //! conceivably use a `std::sync::Arc` to a structure as state, that would definitely be a bad
-//! idea as it would break the rules for actors.
+//! idea as it would break the rules we laid out for actors.
 //!
 //! There is a lot more to learn and explore and your best resource is the test code for Axiom.
 //! The developers have a belief that test code should be well architected and well commented to
-//! act also as a set of examples.
+//! act as a set of examples for users of the library.
 //!
 
 pub mod actors;
