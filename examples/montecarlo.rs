e1ab2026c2b564276873088dbaac8dde05954b35
Fenrir <fenrirwolf@gmail.com>
2019-09-04 00:14:06 -0600

    wip

--- examples/montecarlo.rs
+++ examples/montecarlo.rs
@@ -1,14 +1,125 @@
 //! This is an example of a parallel processing implementation of a Monte-Carlo simulation
-//! to predict the outcome of soccer matches based on the [Dixon-Coles
-//! Model](http://web.math.ku.dk/~rolf/teaching/thesis/DixonColes.pdf).
+//! The simulation is of a basic gambling game adapted from this page:
+//! https://towardsdatascience.com/the-house-always-wins-monte-carlo-simulation-eb82787da2a3
+
+use rand::{thread_rng, Rng};
+use serde::{Serialize, Deserialize};
+
+use std::collections::{HashMap, HashSet};
 
 use axiom::*;
 
-pub struct Data {}
+#[derive(Debug, Clone)]
+struct Game {
+    funds: u32,
+    wager: u32,
+    total_plays: u32,
+}
 
-pub fn main() {
-    // TODO Pending implementation.
+impl Game {
+    fn play(&mut self, ctx: &Context, msg: &Message) -> Status {
+        if let Some(results_aid) = msg.content_as::<ActorId>() {
+            let mut current_play = 1;
+            while current_play <= self.total_plays {
+                match roll_dice() {
+                    true => {
+                        self.funds += self.wager;
+                        current_play += 1;
+                    }
+                    false => {
+                        self.funds -= self.wager;
+                        current_play += 1;
+                    }
+                }
+                results_aid.send_new(GameMsg::new(ctx.aid.clone(), self.funds));
+            }
+            println!("Stopping `Game`...");
+            return Status::Stop;
+        }
+        Status::Processed
+    }
+}
 
+impl Default for Game {
+    fn default() -> Self {
+        Self {
+            funds: 10_000,
+            wager: 100,
+            total_plays: 10,
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct GameMsg {
+    aid: ActorId,
+    funds: u32,
+}
+
+impl GameMsg {
+    fn new(aid: ActorId, funds: u32) -> Self {
+        Self {
+            aid: aid,
+            funds: funds,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct GameResults {
+    results: HashMap<ActorId, Vec<u32>>,
+    monitoring: HashSet<ActorId>,
+}
+
+impl GameResults {
+    fn new() -> Self {
+        Self {
+            results: HashMap::new(),
+            monitoring: HashSet::new(),
+        }
+    }
+}
+
+impl GameResults {
+    fn gather(&mut self, ctx: &Context, msg: &Message) -> Status {
+        if let Some(game_msg) = msg.content_as::<GameMsg>() {
+            // Keep track of all the ActorIds that have sent us messages
+            ActorSystem::current().monitor(&ctx.aid, &game_msg.aid);
+            self.monitoring.insert(game_msg.aid.clone());
+
+            let entry = self.results.entry(game_msg.aid.clone()).or_insert(Vec::new());
+            entry.push(game_msg.funds);
+        } else if let Some(sys_msg) = msg.content_as::<SystemMsg>() {
+            match &*sys_msg {
+                SystemMsg::Stopped(aid) => {
+                    self.monitoring.remove(&aid);
+                    // Send the Stop command only if all the actors talking to us
+                    // have also stopped
+                    if self.monitoring.is_empty() {
+                        println!("{:?}", self.results);
+                        println!("Stopping `GameResults`...");
+                        return Status::Stop;
+                    }
+                },
+                _ => {},
+            }
+        }
+
+        Status::Processed
+    }
+}
+
+fn roll_dice() -> bool {
+    let mut rng = thread_rng();
+    match rng.gen_range(0, 101) {
+        x if x > 51 => true,
+        _ => false,
+    }
+}
+
+const NUM_GAMES: usize = 1;
+
+fn main() {
     // First we initialize the actor system using the default config.
     let config = ActorSystemConfig::default();
     let system = ActorSystem::create(config);
@@ -16,10 +127,19 @@ pub fn main() {
     // Have to do this since we want to call from outside actor system.
     system.init_current();
 
-    // Spawn the actor and send the message.
-    // let aid = system.spawn(true, hello);
-    // aid.send(Message::new(HelloMessages::Greet));
+    // Spawn the Game actors.
+    let game_ids = std::iter::repeat_with(|| system.spawn(Game::default(), Game::play))
+        .take(NUM_GAMES)
+        .collect::<Vec<ActorId>>();
+
+    // Spawn the results aggregator.
+    let results_aid = system.spawn(GameResults::new(), GameResults::gather);
+
+    // Start the game by sending the ID for the GameResults actor to each Game instance
+    for id in game_ids {
+        id.send_new(results_aid.clone());
+    }
 
     // The actor will trigger shutdown, we just wait for it.
-    // system.await_shutdown();
+    system.await_shutdown();
 }
