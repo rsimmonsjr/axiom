df3696731dbd601ce3224361dd3f6a4ffe4b22fc
FenrirWolf <fenrirwolf@gmail.com>
2019-09-14 21:29:53 -0600

    Add a monte carlo simulation under examples (#84)
    * wip
    
    * Mostly-working example, but lots of race conditions
    
    * Spawn Game actors from the GameResults actor
    
    This makes it so that the GameResults actor knows ahead of time how many
    Games will be played, and also cleans up the actor monitoring logic
    
    * Reduce message spam to the GameResults actor
    
    The Game actors now run to completion and send their results all at once.
    Previously they were forcing the GameResults actor to do most of the work,
    which kinda defeats the purpose of parallelization in the first place.
    
    * Replace debug prints with more relevant output
    
    * Update .gitignore
    
    * Update config code to work with latest master

--- examples/montecarlo.rs
+++ examples/montecarlo.rs
@@ -1,25 +1,156 @@
 //! This is an example of a parallel processing implementation of a Monte-Carlo simulation
-//! to predict the outcome of soccer matches based on the [Dixon-Coles
-//! Model](http://web.math.ku.dk/~rolf/teaching/thesis/DixonColes.pdf).
+//! The simulation is of a basic gambling game adapted from this page:
+//! https://towardsdatascience.com/the-house-always-wins-monte-carlo-simulation-eb82787da2a3
+
+use rand::{thread_rng, Rng};
+use serde::{Deserialize, Serialize};
+
+use std::collections::HashMap;
+use std::time::Duration;
 
 use axiom::*;
 
-pub struct Data {}
+#[derive(Debug, Copy, Clone)]
+struct Game {
+    funds: i64,
+    wager: u32,
+    total_plays: u32,
+}
 
-pub fn main() {
-    // TODO Pending implementation.
+impl Game {
+    fn roll_dice() -> bool {
+        let mut rng = thread_rng();
+        match rng.gen_range(0, 101) {
+            x if x > 51 => true,
+            _ => false,
+        }
+    }
 
-    // First we initialize the actor system using the default config.
-    let config = ActorSystemConfig::default();
+    fn play(&mut self, ctx: &Context, msg: &Message) -> Status {
+        // The game starts when this actor receives a go message from the GameResults actor
+        if let Some(results_aid) = msg.content_as::<ActorId>() {
+            let mut current_play = 1;
+            let mut results_vec = Vec::new();
+            while current_play <= self.total_plays {
+                current_play += 1;
+                match Game::roll_dice() {
+                    true => self.funds += self.wager as i64,
+                    false => self.funds -= self.wager as i64,
+                }
+                results_vec.push(self.funds);
+            }
+            results_aid.send_new(GameMsg::new(ctx.aid.clone(), results_vec));
+            return Status::Stop;
+        }
+        Status::Processed
+    }
+}
+
+impl Default for Game {
+    fn default() -> Self {
+        Self {
+            funds: 10_000,
+            wager: 100,
+            total_plays: 100,
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct GameMsg {
+    aid: ActorId,
+    results_vec: Vec<i64>,
+}
+
+impl GameMsg {
+    fn new(aid: ActorId, vec: Vec<i64>) -> Self {
+        Self {
+            aid: aid,
+            results_vec: vec,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct GameResults {
+    games_finished: u32,
+    total_games: u32,
+    results: HashMap<ActorId, Vec<i64>>,
+}
+
+impl GameResults {
+    fn new(total_games: u32) -> Self {
+        Self {
+            games_finished: 0,
+            total_games: total_games,
+            results: HashMap::new(),
+        }
+    }
+}
+
+impl GameResults {
+    fn gather(&mut self, ctx: &Context, msg: &Message) -> Status {
+        // Receive messages from the Game actors and aggregate their results
+        if let Some(game_msg) = msg.content_as::<GameMsg>() {
+            self.results
+                .insert(game_msg.aid.clone(), game_msg.results_vec.clone());
+        }
+
+        if let Some(sys_msg) = msg.content_as::<SystemMsg>() {
+            match &*sys_msg {
+                // This is the first code that will run in the actor. It spawns the Game actors,
+                // registers them to its monitoring list, then sends them a start signal
+                SystemMsg::Start => {
+                    let game_conditions = Game::default();
+                    println!("Starting funds: ${}", game_conditions.funds);
+                    println!("Wager per round: ${}", game_conditions.wager);
+                    println!("Rounds per game: {}", game_conditions.total_plays);
+                    println!("Running simulations...");
+                    for _ in 0..self.total_games {
+                        let aid = ctx.system.spawn(game_conditions, Game::play);
+                        ctx.system.monitor(&ctx.aid, &aid);
+                        aid.send_new(ctx.aid.clone());
+                    }
+                }
+                // This code runs each time a monitored actor stops. Once all Game actors are finished,
+                // the actor system will be shut down.
+                SystemMsg::Stopped(_) => {
+                    self.games_finished += 1;
+                    if self.games_finished == self.total_games {
+                        let average_funds = self
+                            .results
+                            .values()
+                            .map(|v| v.last().unwrap())
+                            .sum::<i64>()
+                            / self.total_games as i64;
+                        println!("Simulations ran: {}", self.results.len());
+                        println!("Final average funds: ${}", average_funds);
+                        ctx.system.trigger_shutdown();
+                    }
+                }
+                _ => {}
+            }
+        }
+        Status::Processed
+    }
+}
+
+const NUM_GAMES: u32 = 100;
+
+fn main() {
+    // Initialize the actor system
+    // FIXME: We spawn an unreasonable number of worker threads here because that magically prevents
+    // a deadlock from happening somehow. Fixing the source of the deadlock would be preferable.
+    let config = ActorSystemConfig {
+        work_channel_size: 100,
+        threads_size: 200,
+        thread_wait_time: Duration::from_millis(100),
+    };
     let system = ActorSystem::create(config);
 
-    // Have to do this since we want to call from outside actor system.
-    system.init_current();
+    // Spawn the results aggregator, which will in turn spawn the Game actors.
+    system.spawn(GameResults::new(NUM_GAMES), GameResults::gather);
 
-    // Spawn the actor and send the message.
-    // let aid = system.spawn(true, hello);
-    // aid.send(Message::new(HelloMessages::Greet));
-
-    // The actor will trigger shutdown, we just wait for it.
-    // system.await_shutdown();
+    // Wait for the actor system to shut down.
+    system.await_shutdown();
 }
